e735a6570619a293bde0c04a4d73a4c2c010afc8
billy6183 <99185733+billy6183@users.noreply.github.com>
2022-03-13 21:30:20 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+        c = a + b;
+        System.out.println(c);
+        System.out.println("Hello, World!");
+    }
+}
